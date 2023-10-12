2bc3f8feae4daa5bca02442a8af42d1c7bf97111
John <28975474+psseuro@users.noreply.github.com>
2023-10-12 14:14:17 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Psseuro, welcome you all to the new site. Please be seated.!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
